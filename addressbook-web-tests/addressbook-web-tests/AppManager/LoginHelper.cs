16f13cbeb1e5ec826325f08cbbb41c1a79a5993b
Nadia <nadezhda.fedorinchik@gmail.com>
2024-08-23 14:36:25 +0200

    Add tests for login and logout

--- addressbook-web-tests/addressbook-web-tests/AppManager/LoginHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/LoginHelper.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using System;
+using System.Security.Principal;
 
 namespace WebAddressbookTests
 {
@@ -9,13 +11,38 @@ namespace WebAddressbookTests
         }
         public void Login(AccountData account)
         {
+            if (IsLoggedIn())
+            {
+                if (IsLoggedIn(account))
+                {
+                    return;
+                }
+
+                Logout();
+            }
             Type(By.Name("user"), account.Username);
             Type(By.Name("pass"), account.Password);
             driver.FindElement(By.XPath("//input[@value='Login']")).Click();
         }
+
+        public bool IsLoggedIn(AccountData account)
+        {
+            return IsLoggedIn()
+                && driver.FindElement(By.Name("logout")).FindElement(By.TagName("b")).Text 
+                    == "(" + account.Username + ")";
+        }
+
+        public bool IsLoggedIn()
+        {
+            return IsElementPresent(By.LinkText("Logout"));
+        }
+
         public void Logout()
         {
-            driver.FindElement(By.LinkText("Logout")).Click();
+            if (IsLoggedIn())
+            {
+                driver.FindElement(By.LinkText("Logout")).Click();
+            }
         }
     }
 }
---
+++ addressbook-web-tests/addressbook-web-tests/Tests/LoginTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class LoginTests : TestBase
+    {
+        
+
+        [Test]
+        public void LoginWithValidCredentialsTest()
+        {
+            app.Auth.Logout();
+
+            AccountData adminAccount = new AccountData("admin", "secret");
+
+            app.Auth.Login(adminAccount);
+
+            Assert.IsTrue(app.Auth.IsLoggedIn(adminAccount));
+        }
+
+        [Test]
+        public void LoginWithInvalidCredentialsTest()
+        {
+            app.Auth.Logout();
+
+            AccountData adminAccount = new AccountData("admin", "invalid_password");
+
+            app.Auth.Login(adminAccount);
+
+            Assert.IsFalse(app.Auth.IsLoggedIn(adminAccount));
+        }
+    }
+}
